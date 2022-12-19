41aa974d4598444ecb36281e85e82fbcf2a991ef
PavithraNS <nspavi1991@gmail.com>
2022-12-19 11:34:16 +0530

    Adding EmployeeAttendance logic

--- EmpWageCompute236Batch/Program.cs
+++ EmpWageCompute236Batch/Program.cs
@@ -11,6 +11,15 @@ namespace EmpWageCompute236Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmplyeeWageCompute");
+            const int IS_PRESENT = 1;
+            //UC-1 Check employee present or not
+            Random random = new Random();
+            int employeeInput = random.Next(0, 2);
+
+            if (IS_PRESENT == employeeInput)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
             Console.ReadLine();
         }
     }
